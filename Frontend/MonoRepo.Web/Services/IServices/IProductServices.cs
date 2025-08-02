3919deb5ff353877ad6840b058b257de7327c706
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-08-02 17:31:42 -0300

    implementa e configura services com HttpClient e injeção de dependencia

---
+++ Frontend/MonoRepo.Web/Services/IServices/IProductServices.cs
@@ -0,0 +1,12 @@
+﻿using MonoRepo.Shared.Data.ValueObjects;
+
+namespace MonoRepo.Web.Services.IServices;
+
+public interface IProductServices
+{
+    Task<ProdutoVo> ObterProdutoPorId(int id);
+    Task<List<ProdutoVo>> ObterTodosProdutos();
+    Task<ProdutoVo> CriarProduto(ProdutoVo produto);
+    Task<ProdutoVo> AtualizarProduto(int id, ProdutoVo produto);
+    Task<bool> DeletarProduto(int id);
+}
