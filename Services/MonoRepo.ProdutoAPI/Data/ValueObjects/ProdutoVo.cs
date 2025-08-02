0dcf1b7d722355cc727f4e6b7a9f4a48b2d39a37
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-08-02 15:19:34 -0300

    feat: adiciona ProdutoVo

---
+++ Services/MonoRepo.ProdutoAPI/Data/ValueObjects/ProdutoVo.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MonoRepo.ProdutoAPI.Data.ValueObjects;
+
+public class ProdutoVo
+{
+    public static ProdutoVo Empty => new ProdutoVo();
+    public static List<ProdutoVo> EmptyList => new List<ProdutoVo>();
+
+    public int? Id { get; set; }
+    public string? NomeProduto { get; set; }
+    public decimal Preco { get; set; }
+    public string? Descricao { get; set; }
+    public string? NomeCategoria { get; set; }
+    public string? ImagemUrl { get; set; }
+}
