47b2e8d9949670254f6aa52083018faecb64f889
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-08-02 16:08:43 -0300

    add projeto para compartilhamento de componentes e recursos entre os serviços

---
+++ Shared/MonoRepo.Shared/Data/ValueObjects/ProdutoVo.cs
@@ -0,0 +1,11 @@
+﻿namespace MonoRepo.Shared.Data.ValueObjects;
+
+public class ProdutoVo : BaseVo<ProdutoVo>
+{
+    public int? Id { get; set; }
+    public string? NomeProduto { get; set; }
+    public decimal Preco { get; set; }
+    public string? Descricao { get; set; }
+    public string? NomeCategoria { get; set; }
+    public string? ImagemUrl { get; set; }
+}
