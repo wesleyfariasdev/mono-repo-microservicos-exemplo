5a520c322b0413132f73781604850e522cbc7a6a
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-08-02 15:19:59 -0300

    feat: adiciona interface para ProdutoRepository

---
+++ Services/MonoRepo.ProdutoAPI/Repository/IRepository/IProdutoRepository.cs
@@ -0,0 +1,12 @@
+﻿using MonoRepo.ProdutoAPI.Data.ValueObjects;
+
+namespace MonoRepo.ProdutoAPI.Repository.IRepository;
+
+public interface IProdutoRepository
+{
+    Task<ProdutoVo> ObterProdutoPorId(int id);
+    Task<List<ProdutoVo>> ObterTodosProdutos();
+    Task<ProdutoVo> CriarProduto(ProdutoVo produto);
+    Task<ProdutoVo> AtualizarProduto(int id, ProdutoVo produto);
+    Task<bool> DeletarProduto(int id);
+}
