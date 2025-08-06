57cdf78d3a99a0f383895df7fd81dbc73ef20c69
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-08-05 22:46:46 -0300

    feat: define rotas para listagem de produtos e detalhes

--- Frontend/MonoRepo.Web/Controllers/HomeController.cs
+++ Frontend/MonoRepo.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MonoRepo.Shared.Data.ValueObjects;
 using MonoRepo.Web.Models;
 using MonoRepo.Web.Services.IServices;
 using System.Diagnostics;
@@ -26,6 +27,31 @@ namespace MonoRepo.Web.Controllers
         {
             var produtos = await _produtoServices.ObterTodosProdutos();
             return View(produtos);
+        }
+
+        public async Task<IActionResult> ProdutoDetalhe(int id)
+        {
+            var produto = await _produtoServices.ObterProdutoPorId(id);
+            return View(produto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CriarProduto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarProduto(ProdutoVo produtoVo)
+        {
+            if (ModelState.IsValid)
+            {
+                var produto = await _produtoServices.CriarProduto(produtoVo);
+                if (produto != null)
+                    return RedirectToAction(nameof(ProdutoDetalhe), new { id = produto.Id });
+
+            }
+            return View();
         }
 
         public IActionResult Privacy()
